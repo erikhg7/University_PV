1ed9217896ae167e7e6a46bc50449b66128c0434
Kelly <kmrebollo3@gmail.com>
2020-02-28 11:56:54 -0700

    separated the months and days

--- Capstone.m
+++ Capstone.m
@@ -8,28 +8,51 @@ plot(GHI)
 
 %% build the load vector
 
-
-% make large load vector full of zeros
-Load = zeros(8640,1);
-Reference = [ January       31
-              February      29
-              March         31
-              April         30
-              May           31
-              June          30
-              July          31
-              August        31
-              October       30
-              November      30
-              December      31;];
+% need to make a time vector, maybe can use the one from Northwestern
+hours_in_day = 24;
+Load = zeros(8640,1);   % make large load vector full of zeros
+Reference = [ January       % this isn't working
+              February      
+              March         
+              April         
+              May           
+              June          
+              July          
+              August        
+              September      % check this number
+              October       
+              November      
+              December      ;];
+          
+month_length =   [  31      % Jan
+                    29      % Feb
+                    31      % March
+                    30      % April
+                    31      % May
+                    30      % Jun
+                    31      % Jul
+                    31      % Aug
+                    30 % Sept check this number
+                    30      % Oct
+                    30      % Nov
+                    31;];   % Dec
               
 % go to the first day in the table
+for n=1:12
+    month_table = Reference(n,1);
+    num_days = Reference(n,2);
+    
+   
+   a = month_table(1,3)
+    
+    
+    %end
+    
+    
+end
 % read through the 24 hours in the table
 % go through every day and add it to the vector
     % might  need to make a vector to look up what month we are in
     %
-
-%% plot(Time,Load)
-
 
 
